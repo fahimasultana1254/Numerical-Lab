c41ce14747de9d3b71dcc2e182994e85def2eb1a
fahimasultana1254 <fahimasultana3010@gmail.com>
2025-08-24 06:20:49 +0600

    Add files via upload

---
+++ Newton Raphson Method.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define EPSILON 0.0001
+using namespace std;
+double func(double x)
+{
+	return x*x*x - x - 11;
+}
+double derivFunc(double x)
+{
+	return 3*x*x ;
+}
+void newtonRaphson(double x)
+{
+	double h = func(x) / derivFunc(x);
+	while (abs(h) >= EPSILON)
+	{
+		h = func(x)/derivFunc(x);
+		x = x - h;
+	}
+	cout << "The value of the root is : " << x <<endl;
+	cout << "Fahima Sultana Sakiya" <<endl;
+}
+int main()
+{
+	double x0 = 2;
+	newtonRaphson(x0);
+	return 0;
+}
+
